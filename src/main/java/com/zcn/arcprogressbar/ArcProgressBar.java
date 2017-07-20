b5e656ff829a8db542fb00a4a5fce08e0f8eda2a
zcn <596785154@qq.com>
2017-07-20 18:13:34 +0800

    增加准确值 防止在中间值不好用

--- src/main/java/com/zcn/arcprogressbar/ArcProgressBar.java
+++ src/main/java/com/zcn/arcprogressbar/ArcProgressBar.java
@@ -4,278 +4,317 @@ package com.zcn.arcprogressbar;
  * Created by Administrator on 2017/6/28.
  */
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BlurMaskFilter;
-import android.graphics.BlurMaskFilter.Blur;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.Paint.Style;
 import android.graphics.RectF;
-import android.graphics.Shader;
-import android.graphics.SweepGradient;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
-public class ArcProgressBar extends View {
+import java.util.ArrayList;
 
-    private Paint outerPaint;
-    private Paint fgPaint;
-    private Paint innerPaint;
-    private float startAngle = 135.0f;//弧度开始点
-    private float sweepAngle = 225.0f;
-    private float arcAcrossAngle = 270.0f;//整个弧度跨越的度数
-    private float bgThickness = 30;//背景弧形条的厚度，即bgPaint的粗度
-    private float fgThickness = 26;
-    /** The width of the view */
-    private int width;
-    /** The height of the view */
-    private int height;
-    /** The circle's center X coordinate */
-    private float cx;
-    /** The circle's center Y coordinate */
-    private float cy;
-    /** The radius of the outer circle */
-    float outerRadius;
-    /** The radius of the inner circle */
-    private float innerRadius;
-    /** The maximum progress amount */
-    private int maxProgress = 5;
-    //private int progress = 3;
-    float left=20.0f;//Rect 左X坐标
-    float right=420.0f;//Rect 右X坐标
-    float top=20f;
-    float bottom=420f;
-    int progress = 3;
-    int i = 0;
+import static java.security.AccessController.getContext;
 
-    int[] colors;
-    Matrix mMatrix;
-    private double markerPointX;
-    private double markerPointY;
+/**
+ * Created by lenovo on 2017/7/15.
+ */
 
-    //private float angleUnit = 180.0f / 256.0f;
-    private float angleUnit  = arcAcrossAngle /maxProgress;;
-    private RectF rect;
-    public Point centerPoint = null;//弧形的圆心点
-    //public float radius = 0;//弧形的半径
-    public double degree;//当前的进度所对应的角度
-    private Context context ;
+public class ArcProgressBar extends ProgressBar{
+    //弧度转角度
+    private static final double RADIAN = 180 / Math.PI;
+    //半径
+    private int mRadius;
+    //画弧的笔
+    private Paint mArcPaint;
+    //画背景颜色的笔
+    private Paint mBackPaint;
+    //弧尽头的小圆点
+    private Paint mPointPaint;
+    //小圆点的X坐标
+    private float mPointX ;
+    //小圆点的Y坐标
+    private float  mPointY ;
+    //小圆点的半径
+    private float mPointRadius;
+    //小圆点的颜色
+    private int mPointColor;
+    //弧线宽度
+    private float mArcWidth;
+    //弧线颜色
+    private int mArcColor;
+    //起始角度,绘制的时候会用到
+    private int mStartAngle;
+    //初始角度
+    private int mCalculateStartAngle;
+    //最大角度
+    private int mMaxAngle;
+    //当前的角度
+    private float mCurrentAngle = 0;
+    //当前进度值
+    private int mCurrentProgerss;
+    //最大进度值
+    private int mMaxProgress;
+    //全部的颜色
+    private int mBackcolor;
 
-    private Handler handler;
+    private OnProgressChangeListener mListener;
 
-    public ArcProgressBar(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        this.context = context;
-        init();
+
+    public ArcProgressBar(Context context)
+    {
+        this(context, null);
     }
 
-    public ArcProgressBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.context = context;
-        init();
+    public ArcProgressBar(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
     }
 
-    public ArcProgressBar(Context context) {
-        super(context);
-        this.context = context;
-        init();
+    public ArcProgressBar(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+        initView(attrs, defStyleAttr);
     }
 
-    public void setMainHandler(Handler handler){
-        this.handler = handler;
+    private void initView(AttributeSet attrs, int defStyleAttr){
+        initAttrs(attrs, defStyleAttr);
+        initPaint();
     }
 
-    /**
-     * Gets the max progress.
-     *
-     * @return the max progress
-     */
-    public int getMaxProgress() {
-        return maxProgress;
+    private void initAttrs(AttributeSet attrs, int defStyleAttr){
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CircleSeekBar, defStyleAttr, 0);
+        mArcWidth = a.getDimension(R.styleable.CircleSeekBar_arc_width, 20);
+        mArcColor = a.getColor(R.styleable.CircleSeekBar_arc_color, Color.BLUE);
+        mBackcolor = a.getColor(R.styleable.CircleSeekBar_back_color,Color.WHITE);
+        mPointRadius = a.getDimension(R.styleable.CircleSeekBar_point_radius, mArcWidth / 2);
+        mPointColor = a.getColor(R.styleable.CircleSeekBar_point_color, Color.GREEN);
+        mStartAngle = a.getInt(R.styleable.CircleSeekBar_start_angle, 135);
+        mMaxAngle = a.getInt(R.styleable.CircleSeekBar_max_angle, 270);
+        mCurrentProgerss = a.getInt(R.styleable.CircleSeekBar_current_progress, 0);
+        mMaxProgress = a.getInt(R.styleable.CircleSeekBar_max_progress, 100);
+        a.recycle();
+        //处理角度偏差
+        mCalculateStartAngle = mStartAngle % 90;
     }
 
-    /**
-     * Sets the max progress.
-     *
-     * @param maxProgress
-     *            the new max progress
-     */
-    public void setMaxProgress(int maxProgress) {
-        this.maxProgress = maxProgress;
-        angleUnit = arcAcrossAngle /maxProgress;
-    }
-
-    public int getProgress() {
-        return progress;
-    }
-
-    public void setProgress(int progress) {
-
-        this.progress = progress;
-    }
-
-    private void init() {
-        //背景Paint
-        outerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        outerPaint.setStyle(Style.STROKE);
-        outerPaint.setStrokeWidth(bgThickness);
-        outerPaint.setColor(Color.WHITE);
-        BlurMaskFilter blurMaskFilter = new BlurMaskFilter(1, Blur.INNER);
-        outerPaint.setMaskFilter(blurMaskFilter);
-
-        //绘制内圆
-        innerPaint = new Paint();
-        innerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        innerPaint.setStyle(Paint.Style.FILL);
-        colors = new int[]{Color.GREEN,Color.BLUE,Color.RED,Color.BLUE,Color.GREEN};
-        mMatrix = new Matrix();
-
-        //前景Paint
-        fgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        fgPaint.setStyle(Style.STROKE);
-        fgPaint.setStrokeWidth(fgThickness);
-        fgPaint.setColor(Color.parseColor("#71dca2"));
-        fgPaint.setStrokeCap(Paint.Cap.ROUND);
-        BlurMaskFilter blurMaskFilter3 = new BlurMaskFilter(1, Blur.OUTER);
-        fgPaint.setMaskFilter(blurMaskFilter3);
-
+    private void initPaint(){
+        //初始化背景的笔
+        mBackPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBackPaint.setColor(mBackcolor);
+        mBackPaint.setStrokeWidth(mArcWidth);
+        mBackPaint.setStyle(Paint.Style.STROKE);
+        //mBackPaint.setStrokeCap(Paint.Cap.ROUND);
+        //初始化弧形的笔
+        mArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mArcPaint.setColor(mArcColor);
+        mArcPaint.setStrokeWidth(mArcWidth);
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        //mArcPaint.setStrokeCap(Paint.Cap.ROUND);
+        //初始化小圆点的笔
+        mPointPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPointPaint.setColor(mPointColor);
+        mPointPaint.setStyle(Paint.Style.FILL);
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        /*int width=MeasureSpec.getSize(widthMeasureSpec);
-        int height=MeasureSpec.getSize(heightMeasureSpec);
-        if(height>250){
-            height=250;
+        int width = measureWidth(widthMeasureSpec);
+        int height = measureHeight(heightMeasureSpec);
+//        //取宽度和高度的最小值
+//        int diameter=Math.min(width,height);
+        setMeasuredDimension(width, height);
+
+    }
+
+    private int measureWidth(int widthMeasureSpec){
+        int specSize = View.MeasureSpec.getSize(widthMeasureSpec);
+        int specMode = View.MeasureSpec.getMode(widthMeasureSpec);
+        int result;
+        if (specMode == View.MeasureSpec.EXACTLY){
+            result = specSize;
+        } else {
+            result = 30;
+            if (specMode == View.MeasureSpec.AT_MOST){
+                result = Math.min(result, specSize);
+            }
         }
-        setMeasuredDimension(width, height);*/
-        width = getWidth(); // Get View Width
-        height = getHeight();// Get View Height
+        return result;
+    }
 
-        int size = (width > height) ? height : width;
-
-        cx = width / 2; // Center X for circle
-        cy = height / 2; // Center Y for circle
-        //modify by chunna.zheng at 2017-06-30c
-        //this attribute can control is the circle is out of layout
-        outerRadius = (size / 2)-50; // Radius of the outer circle / 2;
-        innerRadius = outerRadius - bgThickness/2;//outRadious是向内外扩展
-
-        left = cx - outerRadius; // Calculate left bound of our rect
-        right = cx + outerRadius;// Calculate right bound of our rect
-        top = cy - outerRadius;// Calculate top bound of our rect
-        bottom = cy + outerRadius;// Calculate bottom bound of our rect
-
-        //在此矩形中画弧形进度条
-        rect = new RectF(left, top, right, bottom);
-        centerPoint = new Point((int)cx, (int)cy);
-
-        markerPointX = cx + outerRadius * Math.cos((progress*angleUnit+startAngle) * 3.14 / 180);
-        markerPointY = cy + outerRadius * Math.sin((progress*angleUnit+startAngle) * 3.14 / 180);
-
+    private int measureHeight(int heightMeasureSpec){
+        return measureWidth(heightMeasureSpec);
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh){
         super.onSizeChanged(w, h, oldw, oldh);
-        System.out.println("onSizeChanged");
+        mRadius = Math.min(w - getPaddingLeft() - getPaddingRight(), h - getPaddingTop() - getPaddingBottom()) / 2;
 
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
-
-		/*
-		 * startAngle  开始角度
-		 * sweepAngle  跨越角度
-		 * false  不画中心和弧线的连线
-		 */
-        canvas.drawCircle(cx, cy, outerRadius, outerPaint);
-
-        Shader shader = new SweepGradient(cx,cy,colors,null);
-        mMatrix.setRotate((float) degree, cx, cy);
-        shader.setLocalMatrix(mMatrix);
-        innerPaint.setShader(shader);
-        BlurMaskFilter blurMaskFilter2 = new BlurMaskFilter(1, Blur.OUTER);
-        innerPaint.setMaskFilter(blurMaskFilter2);
-
-        canvas.drawCircle(cx, cy, innerRadius, innerPaint);
-        drawFg(canvas);
-        canvas.drawCircle((float)markerPointX,(float)markerPointY, 20, innerPaint);
+        RectF reactF = new RectF(getPaddingLeft() + mArcWidth, getPaddingTop() + mArcWidth, getPaddingLeft() + 2 * mRadius - mArcWidth, getPaddingTop() + 2 * mRadius - mArcWidth);
+        canvas.drawArc(reactF, mStartAngle, mMaxAngle, false, mBackPaint);
+        canvas.drawArc(reactF, mStartAngle, mCurrentAngle, false, mArcPaint);
+        if (mCurrentAngle >= 0){
+            if (mPointX == 0){
+                mPointX = calculatePointX(true, Math.sin((90 - (float) (90 - Math.asin((double) (321 - mRadius) / (Math.sqrt(Math.pow(94 - mRadius, 2) + Math.pow(321 - mRadius, 2)))) * RADIAN)) / RADIAN));
+                mPointY = calculatePointY((double) (321 - mRadius) / Math.sqrt(Math.pow(94 - mRadius, 2) + Math.pow(321 - mRadius, 2)));;
+            }
+            canvas.drawCircle(mPointX, mPointY, mPointRadius, mPointPaint);
+        }
+    }
+    /**
+     * dip 转换成px
+     * @param dip
+     * @return
+     */
+    private int dipToPx(float dip) {
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return (int)(dip * density + 0.5f * (dip >= 0 ? 1 : -1));
     }
 
     /**
-     * 画Foreground
-     * @param canvas
+     * 得到屏幕宽度
+     * @return
      */
-    private void drawFg(Canvas canvas) {
-        for(i=0;i< progress;i++){
-            canvas.drawArc(rect, startAngle + angleUnit * i, angleUnit, false, fgPaint);
-        }
+    private int getScreenWidth() {
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
     }
 
-    //float coordinateUnit=256/(right-left);
-    float x=0;
-    float y;
-
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
+    public boolean onTouchEvent(MotionEvent event){
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
-                x = event.getX();
-                y = event.getY();
-
-                if(!(x>=left&&x<=right&&y>=top&&y<=(top+outerRadius))){
-                    return true;
+                if (!isValid(x, y)){
+                    return false;
                 }
-
-                degree = Math.toDegrees(Math.atan((y - centerPoint.y)
-                        / (x - centerPoint.x)));
-                if(degree>=0){
-                    progress =(int) Math.round(degree/angleUnit);
-                }else{
-                    progress =(int) Math.round((arcAcrossAngle +degree)/angleUnit);
-                }
-                invalidate();
+                calculateAngle(x, y);
+                calculateProgress();
                 break;
-
             case MotionEvent.ACTION_MOVE:
-                x=event.getX();
-                y = event.getY();
-                if(!(x>=left&&x<=right&&y>=top&&y<=(top+outerRadius))){
-                    return true;
-                }
-
-                degree = Math.toDegrees(Math.atan((y - centerPoint.y)
-                        / (x - centerPoint.x)));
-                if(degree>=0){
-                    progress =(int) Math.round(degree/angleUnit);
-                    markerPointX = cx + outerRadius * Math.cos((progress*angleUnit+startAngle) * 3.14 / 180);
-                    markerPointY = cy + outerRadius * Math.sin((progress*angleUnit+startAngle) * 3.14 / 180);
-                }else{
-                    progress = (int) Math.round((arcAcrossAngle +degree)/angleUnit);
-                    markerPointX = cx + outerRadius * Math.cos((progress*angleUnit+startAngle) * 3.14 / 180);
-                    markerPointY = cy + outerRadius * Math.sin((progress*angleUnit+startAngle) * 3.14 / 180);
-                }
-
-                invalidate();
+                calculateAngle(x, y);
+                calculateProgress();
                 break;
             case MotionEvent.ACTION_UP:
-                Log.d("Chunna.zheng", "=====ACTION_UP=====");
+                calculateAngle(x, y);
+                //添加粘性效果
+                if (mCurrentAngle <= 5){
+                    mCurrentAngle = 0;
+                }
+                calculateProgress();
                 break;
         }
-
+        invalidate();
         return true;
+    }
+
+    private void calculateAngle(float x, float y){
+        float angle;
+        //斜边
+        double hypotenuse;
+        hypotenuse = Math.sqrt(Math.pow(x - mRadius, 2) + Math.pow(y - mRadius, 2));
+        double sin = (double) (y - mRadius) / hypotenuse;
+        float pointX;
+        float pointY;
+        boolean isLeft = x - mRadius < 0;
+        if (isLeft) {
+            angle = (float) (90 - Math.asin(sin) * RADIAN);
+            //计算小圆点坐标
+            pointX = calculatePointX(isLeft, sin);
+            pointY = calculatePointY(sin);
+            Log.d("c_angle", "left:" + angle);
+        } else {
+            angle = (float) (180 + 90 + Math.asin(sin) * RADIAN);
+            //计算小圆点坐标
+            pointX = calculatePointX(isLeft, sin);
+            pointY = calculatePointY(sin);
+            Log.d("c_angle", "right:" + angle);
+        }
+        if (angle >= mCalculateStartAngle && angle <= mMaxAngle + mCalculateStartAngle){
+            mCurrentAngle = Math.round(angle - mCalculateStartAngle);
+            Log.d("cur_angle", "mCurrentAngle:" + mCurrentAngle);
+            mPointX = pointX;
+            mPointY = pointY;
+        }
+    }
+
+    /**
+     * 计算小圆点的X坐标
+     *
+     * @param isLeft 判断点是否位于弧形的左半部分
+     * @param sin    #calculateAngle计算出来的sin值
+     * @return 小圆点的X坐标
+     */
+    private float calculatePointX(boolean isLeft, double sin){
+        return isLeft ? (float) (mRadius - (mRadius - mArcWidth) * Math.sqrt(1 - sin * sin)) + getPaddingLeft() :
+                (float) (mRadius + (mRadius - mArcWidth) * Math.sqrt(1 - sin * sin)) + getPaddingLeft();
+    }
+
+    private float calculatePointY(double sin){
+        return (float) (mRadius + (mRadius - mArcWidth) * sin) + getPaddingTop();
+    }
+
+    private void calculateProgress(){
+        mCurrentProgerss = Math.round(mCurrentAngle / mMaxAngle * mMaxProgress);
+        if (mListener != null)
+        {
+            mListener.onProgress(mCurrentProgerss);
+        }
+    }
+
+    /**
+     * 判断点是否在弧形的半径内
+     *
+     * @param x
+     * @param y
+     * @return true在   false不在
+     */
+    private boolean isValid(float x, float y){
+        return Math.pow(x - mRadius - getPaddingLeft(), 2) + Math.pow(y - mRadius - getPaddingTop(), 2) <= mRadius * mRadius;
+    }
+
+    public void setCurrentProgress(int progress){
+        if (progress > mMaxProgress)
+        {
+            throw new IllegalArgumentException("progress must < mMaxProgress");
+        }
+        mCurrentProgerss = progress;
+        mCurrentAngle = (float) mCurrentProgerss / mMaxProgress * mMaxAngle;
+        float angle = mCurrentAngle + mCalculateStartAngle;
+        //与calculateAngle方法求sin值相反
+        boolean isLeft = angle <= 180;
+        if (isLeft)
+        {
+            mPointX = calculatePointX(isLeft, Math.sin((90 - angle) / RADIAN));
+            mPointY = calculatePointY(Math.sin((90 - angle) / RADIAN));
+        } else
+        {
+            mPointX = calculatePointX(isLeft, Math.sin((angle - 180 - 90) / RADIAN));
+            mPointY = calculatePointY(Math.sin((angle - 180 - 90) / RADIAN));
+        }
+        invalidate();
+    }
+
+    public void setOnProgressChangeListener(OnProgressChangeListener listener){
+        this.mListener = listener;
+    }
+
+    public interface OnProgressChangeListener{
+        void onProgress(int progress);
     }
 }
--- src/main/java/com/zcn/arcprogressbar/MainActivity.java
+++ src/main/java/com/zcn/arcprogressbar/MainActivity.java
@@ -1,13 +1,28 @@
 package com.zcn.arcprogressbar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ArcProgressBar arcProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arcProgressBar = (ArcProgressBar)findViewById(R.id.arcProgressBar);
+        arcProgressBar.setOnProgressChangeListener(new ArcProgressBar.OnProgressChangeListener() {
+            @Override
+            public void onProgress(int progress) {
+                Log.d("Chunna.zheng","output current progress :"+progress);
+            }
+        });
     }
 }
